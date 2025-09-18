985cde7dd08dc2e4518bcbaa917b80d99c18eb59
JesFernandoGL <jesale.07@gmail.com>
2025-09-17 20:38:43 -0600

    Ale Invitacion

---
+++ js.js
@@ -0,0 +1,37 @@
+activateCounter();
+
+const timerInterval = setInterval(function() {
+    activateCounter();
+}, 1000);
+
+
+
+function activateCounter(){
+    const diffTime = new Date('2025-12-04T00:00:00') - new Date();
+    let days = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((diffTime % (1000 * 60)) / 1000);
+    
+    if(`${days}`.length === 1){
+        days = `0${days}`
+    }
+    if(`${hours}`.length === 1){
+        hours = `0${hours}`
+    }
+    if(`${minutes}`.length === 1){
+        minutes = `0${minutes}`
+    }
+    if(`${days}`.length === 1){
+        seconds = `0${seconds}`
+    }
+    
+    $('.DateInv-Counter-Day').text(days);
+    $('.DateInv-Counter-Hrs').text(hours);
+    $('.DateInv-Counter-Min').text(minutes);
+    $('.DateInv-Counter-Seg ').text(seconds);
+
+    if (diffTime <= 0) {
+        clearInterval(timerInterval);
+    }
+}
