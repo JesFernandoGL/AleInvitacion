8338a5bc910f23c0cbe92d7a8aa6bb5ed0f0d27b
JesFernandoGL <jesale.07@gmail.com>
2025-09-18 19:52:18 -0600

    Actualizacion 18Sep

--- js.js
+++ js.js
@@ -7,7 +7,7 @@ const timerInterval = setInterval(function() {
 
 
 function activateCounter(){
-    const diffTime = new Date('2025-12-04T00:00:00') - new Date();
+    const diffTime = new Date('2025-12-06T00:00:00') - new Date();
     let days = Math.floor(diffTime / (1000 * 60 * 60 * 24));
     let hours = Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
